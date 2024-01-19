b9ba9df90fb1aaa133d6bbac11d7095671e0c8da
linth <zhua0404@gmail.com>
2024-01-19 14:38:33 +0800

    doc: add comments.

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 1883;
 
-  // TODO: 升級aedes版本
+  // TODO: 升級aedes版本, 
   // const aedesInstance: Aedes = aedes();
+
+  /** 
+   * CommonJS舊版node.js
+   * ES6 modules: `import xxx from xxx`
+   */
   const aedesInstance = require('aedes')();
+
   const server = createServer(aedesInstance.handle);  
 
   await app.listen(3000);
